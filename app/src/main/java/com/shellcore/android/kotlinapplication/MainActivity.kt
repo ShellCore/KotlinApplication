b90da9fdb2d7131c7ff44b98876f870e17d72584
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-01-26 15:56:34 -0600

    Toast & Snackbars
    - Implementación de mensajes mostrados en la aplicación

--- app/src/main/java/com/shellcore/android/kotlinapplication/MainActivity.kt
+++ app/src/main/java/com/shellcore/android/kotlinapplication/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.shellcore.android.kotlinapplication
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Hello from the toast", Toast.LENGTH_LONG)
+                .show()
+
+        Snackbar.make(container, "Hello from the Snackbar", Snackbar.LENGTH_SHORT)
+                .show()
+
+        Snackbar.make(container, "Hello with action", Snackbar.LENGTH_INDEFINITE)
+                .setAction("Close") {
+                    Snackbar.make(container, "Email has been restored", Snackbar.LENGTH_SHORT)
+                            .show()
+                }
+                .show()
     }
 }
