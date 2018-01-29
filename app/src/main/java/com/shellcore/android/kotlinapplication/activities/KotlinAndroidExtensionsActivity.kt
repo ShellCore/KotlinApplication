ce5d4dfcb505eddd02152d40d84cb396ce8082c4
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-01-29 12:00:44 -0600

    Implementación de onClicks por medio de las extensiones Kotlin para Android

--- app/src/main/java/com/shellcore/android/kotlinapplication/activities/KotlinAndroidExtensionsActivity.kt
+++ app/src/main/java/com/shellcore/android/kotlinapplication/activities/KotlinAndroidExtensionsActivity.kt
@@ -2,12 +2,26 @@ package com.shellcore.android.kotlinapplication.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
 import com.shellcore.android.kotlinapplication.R
+import kotlinx.android.synthetic.main.activity_kotlin_android_extensions.*
 
 class KotlinAndroidExtensionsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_android_extensions)
+
+        val btn = findViewById<Button>(R.id.btnById)
+        btn.setOnClickListener {
+            Toast.makeText(this, "Click by Id", Toast.LENGTH_SHORT)
+                    .show()
+        }
+
+        btnByKat.setOnClickListener {
+            Toast.makeText(this, "Click by KAT", Toast.LENGTH_SHORT)
+                    .show()
+        }
     }
 }
