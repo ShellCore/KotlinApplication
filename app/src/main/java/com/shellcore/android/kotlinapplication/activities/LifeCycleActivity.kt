049f99ff0efd51d6b28aff76e182014818a0a336
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-01-29 09:50:12 -0600

    Implementación de los mensajes para validar los estados del ciclo de vida de un activity.

--- app/src/main/java/com/shellcore/android/kotlinapplication/activities/LifeCycleActivity.kt
+++ app/src/main/java/com/shellcore/android/kotlinapplication/activities/LifeCycleActivity.kt
@@ -1,13 +1,17 @@
 package com.shellcore.android.kotlinapplication.activities
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import com.shellcore.android.kotlinapplication.R
 
-class LifeCycleActivity : AppCompatActivity() {
+class LifeCycleActivity : LifeCycleEventsActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_life_cycle)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
     }
 }
