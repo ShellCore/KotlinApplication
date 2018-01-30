8a6150725e49fa3d114cceed44ec51556de76a2d
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-01-30 10:33:14 -0600

    - Implementación del toolbar para la vista principal de la aplicación - Implementación de toolbar con comportamiento de botón back que retorna al main de la aplicación

--- app/src/main/java/com/shellcore/android/kotlinapplication/MainActivity.kt
+++ app/src/main/java/com/shellcore/android/kotlinapplication/MainActivity.kt
@@ -4,17 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.widget.Toast
 import com.shellcore.android.kotlinapplication.activities.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         showMessages()
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         btnCycle.setOnClickListener { goToLifeCycleActivity() }
         btnEvents.setOnClickListener { goToClickEventsActivity() }
--- app/src/main/java/com/shellcore/android/kotlinapplication/activities/IntentExtrasActivity.kt
+++ app/src/main/java/com/shellcore/android/kotlinapplication/activities/IntentExtrasActivity.kt
@@ -3,6 +3,7 @@ package com.shellcore.android.kotlinapplication.activities
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.view.View
 import com.shellcore.android.kotlinapplication.R
 import com.shellcore.android.kotlinapplication.models.Student
@@ -18,9 +19,15 @@ class IntentExtrasActivity : AppCompatActivity() {
         val STUDENT_KEY = "student"
     }
 
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent_extras)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
         btnBack.setOnClickListener {
             startActivity(Intent(this, IntentsActivity::class.java))
